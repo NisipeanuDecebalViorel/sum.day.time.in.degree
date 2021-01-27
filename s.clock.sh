9b55825d0837d6143a46a784a807cffbe633c5c2
no0ne <nisipeanu.decebal.viorel@gmail.com>
2021-01-27 13:03:02 +0000

    Update s.clock.sh

--- s.clock.sh
+++ s.clock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash                                                                                                                                                    
-printf ' SumerianTime    :  [ %dh°:%dm":%ds` ]\n' $((`date +%s`%86400/7200)) $((`date +%s`%7200/120)) $((`date +%s`%120))
+printf ' SumerianTime    :  [ %dhrs°:%ddeg°":%dsec` ]\n' $((`date +%s`%86400/7200)) $((`date +%s`%7200/240)) $((`date +%s`%240))
